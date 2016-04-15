15ac6bcdc25b8998efe205d63548ac230b881150
Jia Zhang <david.jia.zhang@gmail.com>
2016-04-15 14:34:48 -0400

    let's call it final

--- classify.java
+++ classify.java
@@ -16,13 +16,13 @@ import java.util.Scanner;
  *	A password is considered strong if it meets any one of the following requirements:
  *
  *	1. Comprehensive 8+: The password is at least 8 characters long and must include at least an uppercase letter, a lowercase letter, a symbol, and a digit. 
- *	And the password must not contains any common password sequence and dictionary words predefined in the free Openwall list that is at least 3 characters long.
+ *	And the password must not contains any common password sequence and dictionary words predefined in the free Openwall list that is at least 2 characters long.
  *
  *	2. Basic 16+: The password is at least 16 characters long and must include at least four different characters.
  *
  *	The reason why we choose Comprehensive 8 is that it is proven to be strong in the paper "Guess again (and again and again): Measuring password strength by 
- *	simulating password-cracking algorithms" by Kelley et al. The only difference is that we allows password that contains dictionary words of length 3 characters or less. 
- *	We decide to allow password that contains dictionary words of length 3 characters or less is because in the direction we are using, where are words like "1" and "a".
+ *	simulating password-cracking algorithms" by Kelley et al. The only difference is that we allows password that contains dictionary words of length 2 characters or less. 
+ *	We decide to allow password that contains dictionary words of length 2 characters or less is because in the direction we are using, where are words like "1" and "a".
  *	And it is clearly not correct to say that any password contains an "a" or "1" must be a weak password. 
  *
  *	The reason why we choose Basic 16 is that it is proven to be strong in the paper "Guess again (and again and again): Measuring password strength by 
@@ -80,7 +80,7 @@ class classify {
 			String line;
 			while ((line = reader.readLine()) != null) {
 				// Do not include words that is too short
-				if (line.length() <= 3) continue;
+				if (line.length() <= 2) continue;
 
 				if (strlower.indexOf(line.toLowerCase()) >= 0) {
 					System.out.println("Contain: " + line);
